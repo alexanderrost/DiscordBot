46b9605b388ed8affeb1dedd4190d616c89e67d0
ar223ni <ar223ni@student.lnu.se>
2022-05-05 20:51:10 +0200

    Join and reply function added, doenst work but it's still added :) will continue to work tomorrow Reminder: re-read api and look into game activity.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DiscordBot
 I use discord daily so it's time to get a deeper understanding of the software and API.
+
+```pip install discord```
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello")
+# Created by Alexander Röst 5th of May 2022
+# Please contact me on Github should you have any questions.
+
+import discord
+
+TOKEN = 'hidden'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+client.run(TOKEN)
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}, {user_message}, {channel}')
+
+    if message.author == client.user:
+        return
+
+    if message.channel.name == 'general':
+        if user_message.lower == 'hello':
+            await message.channel.send(f'Hello {username}')
+            return
+        elif user_message.lower == '!random':
+            response = f'this is your random number: 1. f you'
+            await message.channel.send(response)
+            return
+
