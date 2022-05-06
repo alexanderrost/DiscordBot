a667f069d9eaa6b329c6c50cda386a880ab49f5c
ar223ni <ar223ni@student.lnu.se>
2022-05-06 13:03:23 +0200

    Added intents to support members, edited on_ready and added a welcome message.

--- main.py
+++ main.py
@@ -3,32 +3,56 @@
 
 import discord
 
-TOKEN = 'hidden'
+# Your discord token goes here, don't let other people know your token
+TOKEN = ''
 
-client = discord.Client()
+# Intents are the events(actions) our bot is looking for
+# For example we need members to be true in order to have our welcome message work
+intents = discord.Intents(messages=True, guilds=True, members=True)
 
+# Make sure the intents are specified here
+client = discord.Client(intents=intents)
+
+
+# This will be the function run once the bot is ready to use
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.wait_until_ready()
 
-client.run(TOKEN)
 
+# on_message deals with incoming messages and commands
 @client.event
 async def on_message(message):
+    # This gives us just the name. zer0#2569 -> zer0
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
-    print(f'{username}, {user_message}, {channel}')
+    print('{}, {}, {}'.format(username, user_message, channel))
 
+    # This prevents the bot from replying to itself, that could lead to some issues!
     if message.author == client.user:
         return
 
-    if message.channel.name == 'general':
-        if user_message.lower == 'hello':
-            await message.channel.send(f'Hello {username}')
-            return
-        elif user_message.lower == '!random':
-            response = f'this is your random number: 1. f you'
-            await message.channel.send(response)
-            return
+    # Check if the message it saw is &gm and then reply aswell as tag the user
+    if user_message == '&gm':
+        print('test')
+        await message.channel.send('Good Morning G :sunglasses: {}'.format(message.author.mention))
+        return
 
+    elif user_message == '&gn':
+        response = 'Dont let the bedbugs bite :smiling_imp: {}'.format(message.author.mention)
+        await message.channel.send(response)
+        return
+
+# This block will be used to welcome new members
+@client.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    embed = discord.Embed(title="Welcome to the party", description="{} has just joined!".format(member.mention))
+    embed.set_author(name=member, icon_url=member.avatar_url)
+    await channel.send(embed=embed)
+
+# This just starts the client
+# Not sure if this needs to be down here, but i'm to lazy to move it
+client.run(TOKEN)
